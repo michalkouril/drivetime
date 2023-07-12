5c44234e42b3e53736f4c50bb3160b4bce63c580
Michal Kouril <Michal.Kouril@cchmc.org>
2023-07-12 16:28:18 -0400

    Update entrypoint.R

--- entrypoint.R
+++ entrypoint.R
@@ -9,7 +9,7 @@ withr::with_message_sink("/dev/null", library(sf))
 
 doc <- "
       Usage:
-      entrypoint.R <filename> <site>
+      entrypoint.R <filename>
       "
 
 opt <- docopt::docopt(doc)
@@ -18,11 +18,11 @@ opt <- docopt::docopt(doc)
 ## opt <- docopt::docopt(doc, args = 'test/my_address_file_geocoded.csv')
 
 centers <- readr::read_csv('/app/center_addresses.csv')
-selected_site <- opt$site
+# selected_site <- opt$site
 
-if (! selected_site %in% centers$abbreviation){
-  stop('site argument is invalid or missing; please consult documentation for details', call. = FALSE)
-}
+# if (! selected_site %in% centers$abbreviation){
+#  stop('site argument is invalid or missing; please consult documentation for details', call. = FALSE)
+# }
 
 message("reading input file...")
 d <- dht::read_lat_lon_csv(opt$filename, nest_df = T, sf = T, project_to_crs = 5072)
@@ -30,26 +30,37 @@ d <- dht::read_lat_lon_csv(opt$filename, nest_df = T, sf = T, project_to_crs = 5
 dht::check_for_column(d$raw_data, "lat", d$raw_data$lat)
 dht::check_for_column(d$raw_data, "lon", d$raw_data$lon)
 
-message('loading isochrone shape file...')
-isochrones <- readRDS(glue::glue("/app/isochrones/{selected_site}_isochrones.rds")) # 5072 projection
+# FIXME:
 
-## add code here to calculate geomarkers
-message('finding drive time for each point...')
-d$d <- suppressWarnings( st_join(d$d, isochrones, largest = TRUE) ) %>% 
-  mutate(drive_time = ifelse(!is.na(drive_time), as.character(drive_time), "> 60"))
 
-message('finding distance (m) for each point...')
 
-centers <- centers %>% 
-  filter(abbreviation == selected_site) %>% 
-  st_as_sf(coords = c('lon', 'lat'), crs = 4326) %>%
-  st_transform(5072)
 
-d$d$distance <-
-  st_distance(centers,
+lapply(centers, function(selected_site) {
+      
+     message('loading isochrone shape file...')
+     isochrones <- readRDS(glue::glue("/app/isochrones/{selected_site}_isochrones.rds")) # 5072 projection
+
+     ## add code here to calculate geomarkers
+     message('finding drive time for each point...')
+     d$dist[selected_size] <- suppressWarnings( st_join(d$d, isochrones, largest = TRUE) ) %>% 
+       mutate(drive_time = ifelse(!is.na(drive_time), as.character(drive_time), "> 60"))
+
+     message('finding distance (m) for each point...')
+
+     centers <- centers %>% 
+        filter(abbreviation == selected_site) %>% 
+        st_as_sf(coords = c('lon', 'lat'), crs = 4326) %>%
+        st_transform(5072)
+
+     d$d$distance <-
+        st_distance(centers,
               d$d,
               by_element = TRUE)
+}
 
+# now compute the minimal distances for each patient
+#  d$dist[selected_size][]
+       
 ## merge back on .row after unnesting .rows into .row
 dht::write_geomarker_file(d = d$d, 
                           raw_data = d$raw_data, 

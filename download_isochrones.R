e5867b71f2e2a574aab50605b33815cfe4542058
Michal Kouril <xmkouril@gmail.com>
2023-07-19 13:34:05 -0400

    Added 1hour tmp ctsa isochrones

--- download_isochrones.R
+++ download_isochrones.R
@@ -1,19 +1,35 @@
+# options(openrouteservice.url = "http://10.0.1.139:8088/ors")
+# options(openrouteservice.paths = list(directions = "v2/directions",
+#                                       isochrones = "v2/isochrones",
+#                                       matrix = "v2/matrix",
+#                                       geocode = "geocode",
+#                                       pois = "pois",
+#                                       elevation = "elevation",
+#                                       optimization = "optimization"))
+
 library(tidyverse)
 library(sf)
 library(openrouteservice) # remotes::install_github("GIScience/openrouteservice-r")
 
 # read in geocoded facilities data
-centers <- read_csv("center_addresses.csv") %>% 
+# centers <- read_csv("center_addresses.csv") %>% 
+centers <- read_csv("ctsa_centers.csv") %>% 
   arrange(abbreviation)
+
+isochrones <- readRDS("isochrones/isochrones.rds")
 
 # download isochrones from ORS
 get_isochrones <- function(x) {
-  ors_isochrones(centers[x, c('lon', 'lat')],
+  if (length(isochrones) >= x && length(isochrones[[x]]) > 1) {
+    isochrones[[x]]
+  } else {
+  ors_isochrones(as.numeric(centers[x, c('lon', 'lat')]),
                  profile = 'driving-car',
                  range = 60*60,   
                  interval = 6*60,
                  output = "sf") %>%
     st_transform(5072)
+  }
 }
 
 # ex <- get_isochrones(1)
@@ -38,6 +54,17 @@ removeOverlap <- function(x) {
   }
   do.call(rbind, p)
 }
+
+l <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65)
+# ok l <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,36,37,38,39,40,41,42,43,44,45)
+# ok l <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34)
+# ok l <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32)
+# l <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35)
+# l <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40)
+# l <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30)
+
+isochrones <- isochrones[l]
+centers <- centers[l,]
 
 isochrones_no_overlap <- map(isochrones, removeOverlap)
 names(isochrones_no_overlap) <- centers$abbreviation
